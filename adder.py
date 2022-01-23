27ec6fcd72eedd4b2b4feb4da35d9db77f6a8f38
kikoyaja69 <97879683+kikoyaja69@users.noreply.github.com>
2022-01-23 20:48:27 +0700

    Update adder.py

--- adder.py
+++ adder.py
@@ -9,7 +9,7 @@ print ("░█▀█░█▀▄░█▀▄░█▀▀░█▀▄")
 print ("░█▀█░█░█░█░█░█▀▀░█▀▄")
 print ("░▀░▀░▀▀░░▀▀░░▀▀▀░▀░▀")
 print ("")
-print ("      by \033[1;95m@AbirHasan2005")
+print ("      by \033[1;95m@santuylahh")
 print ("\033[1;92m")
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
